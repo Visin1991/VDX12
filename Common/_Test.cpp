3e4e04afbcc957db32b571f490835730f82b582b
Visin1991 <zhuzhanhao1991@gmail.com>
2020-04-22 20:21:36 +0800

    CMake Setpu for DirectX12

---
+++ Common/_Test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MathHelper.h"
+
+
+int main(int argc, char** argv)
+{
+
+	std::cout << MathHelper::RandF() << std::endl;
+
+	system("pause");
+
+	return 0;
+}
---
+++ VDX12_CMake/VDX12_CMake.cpp
@@ -0,0 +1,12 @@
+﻿// VDX12_CMake.cpp : Defines the entry point for the application.
+//
+
+#include "VDX12_CMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
